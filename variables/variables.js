a301d27391f2ed17d39e4083bfabb4e00eea8b50
louis0113 <luizhenrique011304@gmail.com>
2024-12-18 14:48:54 +0000

    Update variables.js

--- variables/variables.js
+++ variables/variables.js
@@ -1,20 +1,37 @@
+
+
+function floatOrInteger (verify){
+
+  if (isFloat(verify)){
+  
+  console.log(`${verify} is a Float Number`)
+  
+} else {
+  console.log(`${verify} is a Integer Number`)
+}
+  
+}
+
 let x = 10 // Integer
 console.log(`${x}`)
+
 x = 20
 console.log(`${x} is a ${typeof(x)}`) //A number includes Integer Numbers and Float Numbers
 
-let hello = "Hello World!" //String
+floatOrInteger(x)
 
+let hello = "Hello World!" //String
 console.log(`${hello} is a ${typeof(hello)}`)
 
 let powerOn = true // Bool 
-
 console.log(`${powerOn} is a ${typeof(powerOn)}`)
 
 let powerOff = !powerOn 
-
 console.log(`${powerOff} is a ${typeof(powerOff)}`)
 
 let y = 10.5 //Float
 
 console.log(`${y} is a ${typeof(y)}`)
+
+floatOrInteger(y)
+
