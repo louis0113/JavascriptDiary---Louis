216a62c7c9265d265ff0bcf43d7867e65c14a49c
louis0113 <luizhenrique011304@gmail.com>
2024-12-19 12:16:23 -0300

    small change :)

--- variables/variables.js
+++ variables/variables.js
@@ -7,26 +7,27 @@ integer ? console.log(`${verify} is a Integer Number \n`) : console.log(`${verif
   
 }
 
+function typeOff (a){
+    console.log(`${a} is a ${typeof(a)}\n`)
+}
+
 let x = 10 // Integer
-console.log(`${x}`)
+
+typeOff(x)
 
 x = 20
-console.log(`${x} is a ${typeof(x)}`) //A number includes Integer Numbers and Float Numbers
+typeOff(x) //A number includes Integer Numbers and Float Numbers
 
 floatOrInteger(x)
 
 let hello = "Hello World!" //String
-console.log(`${hello} is a ${typeof(hello)} \n`)
-
-let powerOn = true // Bool 
-console.log(`${powerOn} is a ${typeof(powerOn)}`)
-
-let powerOff = !powerOn 
-console.log(`${powerOff} is a ${typeof(powerOff) } \n`)
-
+let powerOn = true // Bool = true
+let powerOff = !powerOn //Bool = false
 let y = 10.5 //Float
 
-console.log(`${y} is a ${typeof(y)}`)
-
+typeOff(hello)
+typeOff(powerOn)
+typeOff(powerOff)
+typeOff(y)
 floatOrInteger(y)
 
